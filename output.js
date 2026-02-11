1df1d1f12acd434ffed6802877ec915e24cfeb47
Bintangr191 <bintangrachman27@gmail.com>
2026-02-11 10:21:12 +0700

    feat(output):output from data.js

---
+++ output.js
@@ -0,0 +1,12 @@
+import Planet from './data.js';
+
+console.log(Planet[1].nama, Planet[1].jenis, Planet[1].jarak_matahari, Planet[1].diameter);
+console.log(Planet[2].nama, Planet[2].jenis, Planet[2].jarak_matahari, Planet[2].diameter);
+console.log(Planet[3].nama, Planet[3].jenis, Planet[3].jarak_matahari, Planet[3].diameter); 
+console.log(Planet[4].nama, Planet[4].jenis, Planet[4].jarak_matahari, Planet[4].diameter);
+console.log(Planet[5].nama, Planet[5].jenis, Planet[5].jarak_matahari, Planet[5].diameter);
+console.log(Planet[6].nama, Planet[6].jenis, Planet[6].jarak_matahari, Planet[6].diameter);
+console.log(Planet[7].nama, Planet[7].jenis, Planet[7].jarak_matahari, Planet[7].diameter);
+console.log(Planet[8].nama, Planet[8].jenis, Planet[8].jarak_matahari, Planet[8].diameter);
+console.log(Planet[9].nama, Planet[9].jenis, Planet[9].jarak_matahari, Planet[9].diameter);
+console.log(Planet[9].nama, Planet[10].jenis, Planet[10].jarak_matahari, Planet[10].diameter);
