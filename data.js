b3b7409e3989c69be6bb9d6658034483a8c0d4da
Bintangr191 <bintangrachman27@gmail.com>
2026-02-11 10:10:04 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,74 @@
+const Planet = [
+    {
+        id: 1,
+        nama: 'merkurius',
+        jenis: 'terestrial',
+        jarak_matahari: 0.39,
+        diameter: 4879
+    },
+    {
+        id: 2,
+        nama: 'venus',
+        jenis: 'terestrial',
+        jarak_matahari: 0.72,
+        diameter: 12104
+    },
+    {
+        id: 3,
+        nama: 'bumi',
+        jenis: 'terestrial',
+        jarak_matahari: 1.00,
+        diameter: 12742
+    },
+    {
+        id: 4,
+        nama: 'mars',
+        jenis: 'terestrial',
+        jarak_matahari: 1.50,
+        diameter: 6779
+    },
+    {
+        id: 5,
+        nama: 'jupiter',
+        jenis: 'gas_giant',
+        jarak_matahari: 5.20,
+        diameter: 139820
+    },
+    {
+        id: 6,
+        nama: 'saturnus',
+        jenis: 'gas_giant',
+        jarak_matahari: 9.50,
+        diameter: 116460
+    },
+    {
+        id: 7,
+        nama: 'uranus',
+        jenis: 'ice_giant',
+        jarak_matahari: 19.00,
+        diameter: 50724
+    },
+    {
+        id: 8,
+        nama: 'neptunus',
+        jenis: 'ice_giant',
+        jarak_matahari: 30.00,
+        diameter: 49244
+    },
+    {
+        id: 9,
+        nama: 'pluto',
+        jenis: 'dwarf_planet',
+        jarak_matahari: 39.50,
+        diameter: 2370
+    },
+    {
+        id: 10,
+        nama: 'eris',
+        jenis: 'dwarf_planet',
+        jarak_matahari: 96.30,
+        diameter: 2326
+    }
+]
+
+export default Planet;
